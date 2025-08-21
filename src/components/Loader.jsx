e5d340f9ca175bbe6fa09c450deabffac87a5d1b
vijay vaddi <vijayjay6688@gmail.com>
2025-08-21 17:04:38 +0530

    add loader shurikhen to app

---
+++ src/components/Loader.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './loader.css';
+
+const Loader = () => {
+  return (
+    <div className='loading'>
+      <div className='loader'></div>
+      <div className='loader'></div>
+      <div className='loader'></div>
+    </div>
+  );
+};
+
+export default Loader;
