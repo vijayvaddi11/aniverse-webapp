3871af2a94bbe1ffe7a294872d6af1afcd9a8c3e
vijay vaddi <vijayjay6688@gmail.com>
2025-08-22 21:01:36 +0530

    updated search UI

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,19 +1,31 @@
-import React from 'react'
+export default function Search({ searchTerm, setSearchTerm, onSearch }) {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    onSearch(searchTerm); 
+  };
 
-const Search = ({searchTerm,setSearchTerm}) => {
   return (
-    <div className='search'>
-      <div>
-        <img src='/search.svg' alt='search' />
-        <input
-          type="text"
-          placeholder='Search through thousand of anime titles'
-          value={searchTerm}
-          onChange={(e) =>setSearchTerm(e.target.value)}
-        />
-      </div>
-    </div>
+    <form
+      onSubmit={handleSubmit}
+      className='flex g mt-10 w-full max-w-[800px] mx-auto'
+    >
+      <input
+        type='text'
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
+        placeholder='Search through thousand of animes'
+        className='rounded-l-xl w-full px-4 py-2 border border-gray-700 
+           bg-white/10 backdrop-blur-md text-white placeholder-gray-400 
+           focus:outline-none'
+      />
+      <button
+        type='submit'
+        className='bg-blue-600  px-5 py-2 rounded-r-xl 
+               text-white font-medium shadow-md hover:bg-blue-900 
+               transition'
+      >
+        Search
+      </button>
+    </form>
   );
 }
-
-export default Search
