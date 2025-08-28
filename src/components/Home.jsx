1dce9d784fa3884056c1943af5b3b9b9893f5313
vijay vaddi <vijayjay6688@gmail.com>
2025-08-28 23:10:01 +0530

    added home router

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+ import { useEffect, useState } from 'react';
 import Search from './Search';
 import Loader from './Loader';
 import AnimeCard from './AnimeCard';
